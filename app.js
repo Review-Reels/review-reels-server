8f490151e3625fd11f535cd1b0ef3586eec24014
Hariprasad K B <hariwin7@gmail.com>
2021-07-19 12:35:52 +0530

    review Response api's added

--- app.js
+++ app.js
@@ -19,10 +19,12 @@ app.use(express.json());
 const authRoute = require("./routes/AuthRoute");
 const userRoute = require("./routes/UserRoute");
 const reviewRequestRoute = require("./routes/reviewRequestRoute");
+const reviewResponseRoute = require("./routes/reviewResponseRoute");
 
 //route middleware
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/review", reviewRequestRoute);
+app.use("/api/review", reviewResponseRoute);
 
 app.listen(process.env.PORT, () => console.log("Server Started!"));
---
+++ routes/reviewResponseRoute.js
@@ -0,0 +1,136 @@
+const router = require("express").Router();
+
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+const auth = require("./VerifyToken");
+const { uploadToS3, deleteFromS3, signedUrl } = require("../s3");
+
+router.get("/reviewResponse", auth, async (req, res) => {
+  const { user } = req;
+  console.log(user);
+  try {
+    const currentReviewResponse = await prisma.reviewResponse.findMany({
+      where: {
+        requestMessage: { userId: user.id },
+      },
+    });
+    let requests = [];
+    currentReviewResponse.map((request) => {
+      requests.push(signedUrl(request.videoUrl));
+    });
+    let reviewResponse = [];
+    Promise.all(requests).then((response) => {
+      reviewResponse = currentReviewResponse.map((request, i) => {
+        return { ...request, signedUrl: response[i] };
+      });
+      res.send(reviewResponse);
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
+});
+
+//without auth
+// router.get("/reviewRequest/:username", async (req, res) => {
+//   const { params } = req;
+
+//   try {
+//     const currentReviewRequest = await prisma.reviewRequest.findMany({
+//       where: {
+//         user: { username: params.username },
+//       },
+//     });
+//     let requests = [];
+//     currentReviewRequest.map((request) => {
+//       requests.push(signedUrl(request.videoUrl));
+//     });
+//     let reviewRequests = [];
+//     Promise.all(requests).then((response) => {
+//       reviewRequests = currentReviewRequest.map((request, i) => {
+//         return { ...request, signedUrl: response[i] };
+//       });
+//       res.send(reviewRequests);
+//     });
+//   } catch (e) {
+//     console.log(e);
+//     res.status(400).json(e);
+//   }
+// });
+router.post("/reviewResponse", async (req, res) => {
+  const { files, body } = req;
+  const { name, data, size } = files.fileName;
+  const { whatYouDo, customerName, reviewRequestId } = body;
+
+  const s3FileName = `${reviewRequestId}/${name}`;
+  try {
+    const uploadResponse = await uploadToS3(s3FileName, data);
+
+    const currentReviewResponse = await prisma.reviewResponse.create({
+      data: {
+        customerName,
+        whatYouDo,
+        size,
+        videoUrl: s3FileName,
+        requestMessageId: reviewRequestId,
+      },
+    });
+    currentReviewResponse.signedUrl = await signedUrl(s3FileName);
+
+    res.send(currentReviewResponse);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
+});
+
+// router.put("/reviewRequest/:id", auth, async (req, res) => {
+//   const { files, body, user, params } = req;
+//   const { name, data, size } = files.fileName;
+//   const { askMessage } = body;
+
+//   const s3FileName = `${user.id}/${name}`;
+//   try {
+//     const uploadResponse = await uploadToS3(s3FileName, data);
+
+//     const currentReviewRequest = await prisma.reviewRequest.update({
+//       where: {
+//         id: params.id,
+//       },
+//       data: {
+//         askMessage,
+//         size,
+//         videoUrl: s3FileName,
+//         userId: user.id,
+//       },
+//     });
+//     currentReviewRequest.signedUrl = await signedUrl(s3FileName);
+
+//     res.send(currentReviewRequest);
+//   } catch (e) {
+//     console.log(e);
+//     res.status(400).json(e);
+//   }
+// });
+
+// router.delete("/reviewRequest/:id", auth, async (req, res) => {
+//   const { params } = req;
+
+//   try {
+//     const currentReviewRequest = await prisma.reviewRequest.delete({
+//       where: {
+//         id: params.id,
+//       },
+//     });
+//     const s3FileName = currentReviewRequest.videoUrl;
+//     const uploadResponse = await deleteFromS3(s3FileName);
+
+//     res.status(201);
+//   } catch (e) {
+//     console.log(e);
+//     res.status(400).json(e);
+//   }
+// });
+
+module.exports = router;
