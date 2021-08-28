e39531cd8391b969e2b08995d4a679064d45c90a
Hariprasad K B <hariwin7@gmail.com>
2021-08-28 14:44:24 +0530

    email sending complete

--- routes/emailRoute.js
+++ routes/emailRoute.js
@@ -24,29 +24,51 @@ const mailgunAuth = {
 };
 
 router.post("/sendMail", auth, async (req, res) => {
-  const { subject, sendTo } = req.body;
+  const { subject, sendTo, reviewRequestId } = req.body;
   const smtpTransport = nodemailer.createTransport(mg(mailgunAuth));
 
   const template = handlebars.compile(emailTemplateSource);
 
-  const htmlToSend = template({ message: "Hello!" });
+  try {
+    const reviewRequest = await prisma.reviewRequest.findUnique({
+      where: {
+        id: reviewRequestId,
+      },
+    });
 
-  const mailOptions = {
-    from: "requests@reviewreels.app",
-    to: sendTo,
-    subject: subject,
-    html: htmlToSend,
-  };
+    let mailsToSend = [];
+    sendTo.map(async (sender) => {
+      const htmlToSend = template({
+        askMessage: reviewRequest.askMessage,
+        reviewRequestUrl: `${process.env.WEB_APP_URL}${req.user.username}`,
+        imageUrl: `${process.env.S3_URL}${reviewRequest.imageUrl}`,
+      });
 
-  smtpTransport.sendMail(mailOptions, function (error, response) {
-    if (error) {
-      console.log(error);
-      res.send({ message: error });
-    } else {
-      console.log("Successfully sent email.");
-      res.send({ message: "successfully send email" });
-    }
-  });
+      const mailOptions = {
+        from: "requests@reviewreels.app",
+        to: sender,
+        subject: subject,
+        html: htmlToSend,
+      };
+      mailsToSend.push(smtpTransport.sendMail(mailOptions));
+    });
+
+    Promise.allSettled(mailsToSend).then((results) => {
+      let sendMails = [];
+      let errorMails = [];
+      results.forEach((result, num) => {
+        if (result.status == "fulfilled") {
+          sendMails.push(sendTo[num]);
+        }
+        if (result.status == "rejected") {
+          errorMails.push(sendTo[num]);
+        }
+      });
+      res.send({ message: "send email", sendMails, errorMails });
+    });
+  } catch (err) {
+    res.send({ message: "Some Error occured", err });
+  }
 });
 
 module.exports = router;
