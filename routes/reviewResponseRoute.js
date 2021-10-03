9c649733fed87813ca1dd6aeb0a245d551b5309b
Hariprasad K B <hariwin7@gmail.com>
2021-10-03 09:36:42 +0530

    update review response bug fix:

--- routes/reviewResponseRoute.js
+++ routes/reviewResponseRoute.js
@@ -58,15 +58,31 @@ router.post("/reviewResponse", async (req, res) => {
 });
 
 router.put("/reviewResponse/:id", auth, async (req, res) => {
-  const { body, user, params } = req;
-
+  const { body, user, params, files } = req;
+  let dataTosend = { ...body };
   try {
+    if (files && files.fileName) {
+      const { data, size } = files.fileName;
+      const { whatYouDo, customerName, reviewRequestId } = body;
+      const name = new Date().toISOString();
+      const s3FileName = `${reviewRequestId}/${name}`;
+      await upload(s3FileName, data);
+      dataTosend = {
+        ...dataTosend,
+        customerName,
+        whatYouDo,
+        size,
+        videoUrl: s3FileName + ".mp4",
+        imageUrl: s3FileName + ".jpg",
+      };
+    }
+
     const currentReviewResponse = await prisma.reviewResponse.update({
       where: {
         id: params.id,
       },
       data: {
-        ...body,
+        ...dataTosend,
       },
     });
 
