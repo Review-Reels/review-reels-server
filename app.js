5ee2f8b79201dcbfd6b48ec36c4311eaf452daae
Hariprasad K B <hariwin7@gmail.com>
2022-08-04 10:59:46 +0530

    subscription

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(
 app.use(cors());
 dotenv.config();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // support encoded bodies
 
 //import routes
 const authRoute = require("./routes/AuthRoute");
@@ -21,6 +22,7 @@ const userRoute = require("./routes/UserRoute");
 const reviewRequestRoute = require("./routes/reviewRequestRoute");
 const reviewResponseRoute = require("./routes/reviewResponseRoute");
 const emailRoute = require("./routes/emailRoute");
+const subscriptionRoute = require("./routes/subscriptionRoute");
 
 //route middleware
 app.use("/api/auth", authRoute);
@@ -28,5 +30,73 @@ app.use("/api/user", userRoute);
 app.use("/api/review", reviewRequestRoute);
 app.use("/api/review", reviewResponseRoute);
 app.use("/api/email", emailRoute);
+app.use("/api/subscription", subscriptionRoute);
+
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  (request, response) => {
+    let event = request.body;
+    // Replace this endpoint secret with your endpoint's unique secret
+    // If you are testing with the CLI, find the secret by running 'stripe listen'
+    // If you are using an endpoint defined with the API or dashboard, look in your webhook settings
+    // at https://dashboard.stripe.com/webhooks
+    const endpointSecret = "whsec_12345";
+    // Only verify the event if you have an endpoint secret defined.
+    // Otherwise use the basic event deserialized with JSON.parse
+    if (endpointSecret) {
+      // Get the signature sent by Stripe
+      const signature = request.headers["stripe-signature"];
+      try {
+        event = stripe.webhooks.constructEvent(
+          request.body,
+          signature,
+          endpointSecret
+        );
+      } catch (err) {
+        console.log(`⚠️  Webhook signature verification failed.`, err.message);
+        return response.sendStatus(400);
+      }
+    }
+    let subscription;
+    let status;
+    // Handle the event
+    switch (event.type) {
+      case "customer.subscription.trial_will_end":
+        subscription = event.data.object;
+        status = subscription.status;
+        console.log(`Subscription status is ${status}.`);
+        // Then define and call a method to handle the subscription trial ending.
+        // handleSubscriptionTrialEnding(subscription);
+        break;
+      case "customer.subscription.deleted":
+        subscription = event.data.object;
+        status = subscription.status;
+        console.log(`Subscription status is ${status}.`);
+        // Then define and call a method to handle the subscription deleted.
+        // handleSubscriptionDeleted(subscriptionDeleted);
+        break;
+      case "customer.subscription.created":
+        subscription = event.data.object;
+        status = subscription.status;
+        console.log(`Subscription status is ${status}.`);
+        // Then define and call a method to handle the subscription created.
+        // handleSubscriptionCreated(subscription);
+        break;
+      case "customer.subscription.updated":
+        subscription = event.data.object;
+        status = subscription.status;
+        console.log(`Subscription status is ${status}.`);
+        // Then define and call a method to handle the subscription update.
+        // handleSubscriptionUpdated(subscription);
+        break;
+      default:
+        // Unexpected event type
+        console.log(`Unhandled event type ${event.type}.`);
+    }
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  }
+);
 
 app.listen(process.env.PORT, () => console.log("Server Started!"));
