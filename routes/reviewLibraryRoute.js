168776a41d09de0c9d4e81d845df64ebe1f0fbbe
Hariprasad K B <hariwin7@gmail.com>
2022-12-14 22:17:41 +0530

    library checkpoint

--- routes/reviewLibraryRoute.js
+++ routes/reviewLibraryRoute.js
@@ -22,12 +22,54 @@ router.get("/libraryList", auth, async (req, res) => {
   }
 });
 
+router.get("/libraryOne/:id", auth, async (req, res) => {
+  const { user, params } = req;
+
+  try {
+    const reviewLibrary = await prisma.reviewLibrary.findUnique({
+      where: {
+        id: params.id,
+      },
+    });
+    console.log(reviewLibrary);
+    const reviewResponses = await prisma.reviewResponse.findMany({
+      where: {
+        id: { in: reviewLibrary.libraryConfigJson },
+      },
+      orderBy: { createdAt: "desc" },
+    });
+    res.send({ reviewLibrary, reviewResponses });
+  } catch (e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
+});
+
 router.post("/librarySave", auth, async (req, res) => {
   try {
     const { user, body } = req;
-    console.log(body);
 
     const createdLibraryInstance = await prisma.reviewLibrary.create({
+      data: {
+        ...body,
+        userId: user.id,
+      },
+    });
+    res.send(createdLibraryInstance);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
+});
+
+router.put("/libraryEdit/:id", auth, async (req, res) => {
+  try {
+    const { user, body, params } = req;
+
+    const createdLibraryInstance = await prisma.reviewLibrary.update({
+      where: {
+        id: params.id,
+      },
       data: {
         ...body,
         userId: user.id,
